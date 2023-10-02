f740fbf630fe76398deed89d9d62105bb296c6c8
Yakitori05 <144098931+Yakitori05@users.noreply.github.com>
2023-10-02 10:57:50 +0200

    commit4

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject platformPrefab;
+
+    public int platformcount = 300;
+
     void Start()
     {
-        
-    }
+        Vector3 spawnposition = new Vector3();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int i = 0; i < platformcount; i++)
+        {
+            spawnposition.y += Random.Range(.5f, 2f);
+            spawnposition.x = Random.Range(-5f, 5f);
+            Instantiate(platformPrefab, spawnposition, Quaternion.identity);
+        }
     }
 }
